4363dda8b48084b016167d581d5f7b6e71a9b718
Yudhapermanaa <203040101@mail.unpas.ac.id>
2022-10-27 09:14:00 +0700

    Video 8 (Post Category dan Eloquent Relationship)

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,6 +5,7 @@
 @section('container')
     <article>
         <h2>{{ $post->title }}</h2>
+        <p>By. Yudha permana in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
         {!! $post->body !!}
     </article>
     <a href="/blog">Back To Post</a>
